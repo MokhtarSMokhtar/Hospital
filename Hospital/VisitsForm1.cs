2e1fed2aeacebd4f94a3ee32cc41ba97ed9b0562
Mokhtar S .Mokhtar <Mokhtarsayed72@outlook.com>
2022-10-07 13:32:04 +0200

    last

--- Hospital/VisitsForm1.cs
+++ Hospital/VisitsForm1.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Hospital.Classes;
-using Hospital.Data;
+using Hospital;
 using Hospital.Classes;
 
 namespace Hospital
