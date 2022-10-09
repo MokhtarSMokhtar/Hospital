fc087e9ae24f11c67e100a019be1fd9edd7336e5
Ahmed <Ahmed@AHMED-PC>
2022-10-09 20:43:55 +0200

    Room

--- Hospital/Form1.Designer.cs
+++ Hospital/Form1.Designer.cs
@@ -30,13 +30,29 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MainPanal = new System.Windows.Forms.Panel();
+            this.DoctorPanal = new System.Windows.Forms.Panel();
             this.DoctorAndPatientbtn = new System.Windows.Forms.Button();
             this.PatientStateBtn = new System.Windows.Forms.Button();
             this.DoctorBtn = new System.Windows.Forms.Button();
             this.PatientBtn = new System.Windows.Forms.Button();
             this.RoomBtn = new System.Windows.Forms.Button();
-            this.DoctorPanal = new System.Windows.Forms.Panel();
+            this.RoomPanel = new System.Windows.Forms.Panel();
+            this.Room = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.MainPanal.SuspendLayout();
+            this.DoctorPanal.SuspendLayout();
+            this.RoomPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainPanal
@@ -52,10 +68,18 @@
             this.MainPanal.Controls.Add(this.DoctorBtn);
             this.MainPanal.Controls.Add(this.PatientBtn);
             this.MainPanal.Controls.Add(this.RoomBtn);
-            this.MainPanal.Location = new System.Drawing.Point(-4, 1);
+            this.MainPanal.Location = new System.Drawing.Point(-4, 2);
             this.MainPanal.Name = "MainPanal";
-            this.MainPanal.Size = new System.Drawing.Size(816, 474);
+            this.MainPanal.Size = new System.Drawing.Size(816, 473);
             this.MainPanal.TabIndex = 0;
+            // 
+            // DoctorPanal
+            // 
+            this.DoctorPanal.Controls.Add(this.RoomPanel);
+            this.DoctorPanal.Location = new System.Drawing.Point(0, 0);
+            this.DoctorPanal.Name = "DoctorPanal";
+            this.DoctorPanal.Size = new System.Drawing.Size(816, 471);
+            this.DoctorPanal.TabIndex = 5;
             // 
             // DoctorAndPatientbtn
             // 
@@ -102,12 +126,134 @@
             this.RoomBtn.Text = "Room";
             this.RoomBtn.UseVisualStyleBackColor = true;
             // 
-            // DoctorPanal
+            // RoomPanel
             // 
-            this.DoctorPanal.Location = new System.Drawing.Point(0, 0);
-            this.DoctorPanal.Name = "DoctorPanal";
-            this.DoctorPanal.Size = new System.Drawing.Size(816, 471);
-            this.DoctorPanal.TabIndex = 5;
+            this.RoomPanel.Controls.Add(this.button4);
+            this.RoomPanel.Controls.Add(this.button3);
+            this.RoomPanel.Controls.Add(this.button2);
+            this.RoomPanel.Controls.Add(this.button1);
+            this.RoomPanel.Controls.Add(this.textBox4);
+            this.RoomPanel.Controls.Add(this.textBox3);
+            this.RoomPanel.Controls.Add(this.textBox2);
+            this.RoomPanel.Controls.Add(this.textBox1);
+            this.RoomPanel.Controls.Add(this.label4);
+            this.RoomPanel.Controls.Add(this.label3);
+            this.RoomPanel.Controls.Add(this.label2);
+            this.RoomPanel.Controls.Add(this.label1);
+            this.RoomPanel.Controls.Add(this.Room);
+            this.RoomPanel.Location = new System.Drawing.Point(3, 0);
+            this.RoomPanel.Name = "RoomPanel";
+            this.RoomPanel.Size = new System.Drawing.Size(813, 473);
+            this.RoomPanel.TabIndex = 0;
+            // 
+            // Room
+            // 
+            this.Room.AutoSize = true;
+            this.Room.Location = new System.Drawing.Point(14, 12);
+            this.Room.Name = "Room";
+            this.Room.Size = new System.Drawing.Size(34, 13);
+            this.Room.TabIndex = 0;
+            this.Room.Text = "Room";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Room ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Room Nurse";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Number Of Bed";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Room Type";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 254);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(385, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Visits";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Doctor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(385, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Patient";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -120,6 +266,9 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.MainPanal.ResumeLayout(false);
+            this.DoctorPanal.ResumeLayout(false);
+            this.RoomPanel.ResumeLayout(false);
+            this.RoomPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -133,6 +282,20 @@
         private System.Windows.Forms.Button PatientBtn;
         private System.Windows.Forms.Button RoomBtn;
         private System.Windows.Forms.Panel DoctorPanal;
+        private System.Windows.Forms.Panel RoomPanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Room;
     }
 }
 
