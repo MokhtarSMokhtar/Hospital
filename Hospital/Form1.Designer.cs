dd8db060fe9cbb0427efacb030f4f5d420f7132b
Ali <Ali@DESKTOP-FMV0QPU>
2022-10-09 18:37:03 -0700

    image

--- Hospital/Form1.Designer.cs
+++ Hospital/Form1.Designer.cs
@@ -28,12 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.MainPanal = new System.Windows.Forms.Panel();
-            this.RoomBtn = new System.Windows.Forms.Button();
-            this.PatientBtn = new System.Windows.Forms.Button();
-            this.DoctorBtn = new System.Windows.Forms.Button();
-            this.PatientStateBtn = new System.Windows.Forms.Button();
             this.DoctorAndPatientbtn = new System.Windows.Forms.Button();
+            this.PatientStateBtn = new System.Windows.Forms.Button();
+            this.DoctorBtn = new System.Windows.Forms.Button();
+            this.PatientBtn = new System.Windows.Forms.Button();
+            this.RoomBtn = new System.Windows.Forms.Button();
             this.MainPanal.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,51 +43,17 @@
             this.MainPanal.AllowDrop = true;
             this.MainPanal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.MainPanal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MainPanal.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MainPanal.BackgroundImage")));
+            this.MainPanal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.MainPanal.Controls.Add(this.DoctorAndPatientbtn);
             this.MainPanal.Controls.Add(this.PatientStateBtn);
             this.MainPanal.Controls.Add(this.DoctorBtn);
             this.MainPanal.Controls.Add(this.PatientBtn);
             this.MainPanal.Controls.Add(this.RoomBtn);
-            this.MainPanal.Location = new System.Drawing.Point(0, 1);
+            this.MainPanal.Location = new System.Drawing.Point(-4, 1);
             this.MainPanal.Name = "MainPanal";
-            this.MainPanal.Size = new System.Drawing.Size(813, 486);
+            this.MainPanal.Size = new System.Drawing.Size(816, 372);
             this.MainPanal.TabIndex = 0;
-            // 
-            // RoomBtn
-            // 
-            this.RoomBtn.Location = new System.Drawing.Point(699, 11);
-            this.RoomBtn.Name = "RoomBtn";
-            this.RoomBtn.Size = new System.Drawing.Size(85, 23);
-            this.RoomBtn.TabIndex = 0;
-            this.RoomBtn.Text = "Room";
-            this.RoomBtn.UseVisualStyleBackColor = true;
-            // 
-            // PatientBtn
-            // 
-            this.PatientBtn.Location = new System.Drawing.Point(589, 11);
-            this.PatientBtn.Name = "PatientBtn";
-            this.PatientBtn.Size = new System.Drawing.Size(86, 23);
-            this.PatientBtn.TabIndex = 1;
-            this.PatientBtn.Text = "Patient";
-            this.PatientBtn.UseVisualStyleBackColor = true;
-            // 
-            // DoctorBtn
-            // 
-            this.DoctorBtn.Location = new System.Drawing.Point(475, 12);
-            this.DoctorBtn.Name = "DoctorBtn";
-            this.DoctorBtn.Size = new System.Drawing.Size(85, 23);
-            this.DoctorBtn.TabIndex = 2;
-            this.DoctorBtn.Text = "Doctor";
-            this.DoctorBtn.UseVisualStyleBackColor = true;
-            // 
-            // PatientStateBtn
-            // 
-            this.PatientStateBtn.Location = new System.Drawing.Point(219, 11);
-            this.PatientStateBtn.Name = "PatientStateBtn";
-            this.PatientStateBtn.Size = new System.Drawing.Size(112, 23);
-            this.PatientStateBtn.TabIndex = 3;
-            this.PatientStateBtn.Text = "Patient State";
-            this.PatientStateBtn.UseVisualStyleBackColor = true;
             // 
             // DoctorAndPatientbtn
             // 
@@ -97,11 +64,47 @@
             this.DoctorAndPatientbtn.Text = "Doctor And Patient";
             this.DoctorAndPatientbtn.UseVisualStyleBackColor = true;
             // 
+            // PatientStateBtn
+            // 
+            this.PatientStateBtn.Location = new System.Drawing.Point(219, 11);
+            this.PatientStateBtn.Name = "PatientStateBtn";
+            this.PatientStateBtn.Size = new System.Drawing.Size(112, 23);
+            this.PatientStateBtn.TabIndex = 3;
+            this.PatientStateBtn.Text = "Patient State";
+            this.PatientStateBtn.UseVisualStyleBackColor = true;
+            // 
+            // DoctorBtn
+            // 
+            this.DoctorBtn.Location = new System.Drawing.Point(475, 12);
+            this.DoctorBtn.Name = "DoctorBtn";
+            this.DoctorBtn.Size = new System.Drawing.Size(85, 23);
+            this.DoctorBtn.TabIndex = 2;
+            this.DoctorBtn.Text = "Doctor";
+            this.DoctorBtn.UseVisualStyleBackColor = true;
+            // 
+            // PatientBtn
+            // 
+            this.PatientBtn.Location = new System.Drawing.Point(589, 11);
+            this.PatientBtn.Name = "PatientBtn";
+            this.PatientBtn.Size = new System.Drawing.Size(86, 23);
+            this.PatientBtn.TabIndex = 1;
+            this.PatientBtn.Text = "Patient";
+            this.PatientBtn.UseVisualStyleBackColor = true;
+            // 
+            // RoomBtn
+            // 
+            this.RoomBtn.Location = new System.Drawing.Point(699, 11);
+            this.RoomBtn.Name = "RoomBtn";
+            this.RoomBtn.Size = new System.Drawing.Size(85, 23);
+            this.RoomBtn.TabIndex = 0;
+            this.RoomBtn.Text = "Room";
+            this.RoomBtn.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(814, 485);
+            this.ClientSize = new System.Drawing.Size(812, 385);
             this.Controls.Add(this.MainPanal);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
