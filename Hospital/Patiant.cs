b7eb0297d63ec4c8e3fa621dcba70c5af30d0280
Ali <Ali@DESKTOP-FMV0QPU>
2022-10-12 14:12:42 -0700

    patient

---
+++ Hospital/Patiant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hospital
+{
+    public partial class Patiant : Form
+    {
+        public Patiant()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ID_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Patiant_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
