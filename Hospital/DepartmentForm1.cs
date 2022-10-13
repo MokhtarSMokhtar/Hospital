90b5a73a897e676838638a5b260400edb4a3fb83
mekhaaaa <70482286+mekhaaaa@users.noreply.github.com>
2022-10-13 12:38:51 +0100

    AddDatabase

--- Hospital/DepartmentForm1.cs
+++ Hospital/DepartmentForm1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hospital.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,15 +8,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hospital.Data;
+using System.Data.Entity.Core.Objects.DataClasses;
 
 namespace Hospital
 {
     public partial class DepartmentForm1 : Form
     {
         MainForm mainForm;
-        public DepartmentForm1(MainForm main)
+        Context context;
+        
+        public DepartmentForm1(Context _context,MainForm main )
         {
             this.mainForm = main;   
+            context = _context; 
             InitializeComponent();
             this.FormClosed += DepartmentForm1_FormClosed;
 
@@ -41,6 +47,27 @@ namespace Hospital
         private void AddDeptbtn_Click(object sender, EventArgs e)
         {
 
+            
+            try
+            {
+                var DoctorId = Convert.ToInt32(DeptManagerTextbox.Text);
+                var Dept = new Department
+                {
+                    Name = this.DeptNameTextbox.Text,
+                    ID = Convert.ToInt32(DeptIdTextbox.Text),
+                    Manager = context.Doctors.Where(d => d.ID == DoctorId).FirstOrDefault(),
+                    
+
+                };
+                Dept.Manager.DepartmentManageId = Dept.ID;
+                Dept.Manager.DepartmentWorkId = Dept.ID;
+                context.Departments.Add(Dept);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error");
+            }
         }
 
         private void UpdateDeptBtn_Click(object sender, EventArgs e)
